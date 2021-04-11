e66f0771fbdf14b9fd2c05b9b0140e3a6d958953
andrewrico <andrewric86@gmail.com>
2021-04-11 13:51:33 -0400

    aria plus js

--- drawer-aside/scripts.js
+++ drawer-aside/scripts.js
@@ -1,32 +1,117 @@
 document.body.classList.toggle('js-enable');
 
-// Content is loaded
-document.addEventListener("DOMContentLoaded", function (event) {
+// ContentLoaded
+document.addEventListener("DOMContentLoaded", function () {
 
-    // Menu Items
-    let hamburger =  document.getElementById("hamburger");
+    const showMenuTrigger = document.querySelectorAll('input[type=checkbox][id=drawer__menu]');
+    const hamburger = document.getElementById('hamburger');
+    const item1 = document.getElementById('item1');
+    const item2 = document.getElementById('item2');
+    const item3 = document.getElementById('item3');
+    const tab1 = document.getElementById('tab1');
+    const tab2 = document.getElementById('tab2');
+    const tab3 = document.getElementById('tab3');
+    
+    item1.onclick = function () {
+        showMenuTrigger.forEach(menu => 
+        menu.addEventListener('change', function (event) {
+            if (!event.target.checked || !item1.getAttribute("aria-expanded") == "true") {
+                item1.setAttribute("aria-expanded", "false");
+            } else  {
+                item1.setAttribute("aria-expanded", "true");
+            }
+        })
+    )};
 
-    // Click Event
-    hamburger.onclick = function() {
+    item2.onclick = function () {
+        showMenuTrigger.forEach(menu => 
+        menu.addEventListener('change', function (event) {
+            if (!event.target.checked || !item2.getAttribute("aria-expanded") == "true") {
+                item2.setAttribute("aria-expanded", "false");
+            } else  {
+                item2.setAttribute("aria-expanded", "true");
+            }
+        })
+    )};
 
-        // Open Drawer Trigger Checkbox
-        let drawer__menu = document.querySelectorAll('input[type=checkbox][id=drawer__menu]');
 
-        // Looping Drawers
-        for (var drawer of drawer__menu) {
+    item3.onclick = function () {
+        showMenuTrigger.forEach(menu => 
+        menu.addEventListener('change', function (event) {
+            if (!event.target.checked || !item3.getAttribute("aria-expanded") == "true") {
+                item3.setAttribute("aria-expanded", "false");
+            } else  {
+                item3.setAttribute("aria-expanded", "true");
+            }
+        })
+    )};
 
-            // On change event
-            drawer.addEventListener('change', function(event) {
-
-            // Conditional
+    hamburger.onclick = function () {
+        showMenuTrigger.forEach(menu => 
+        menu.addEventListener('change', function (event) {
             if (!event.target.checked || !hamburger.getAttribute("aria-expanded") == "true") {
-                    hamburger.setAttribute("aria-expanded", "false");
-                    hamburger.setAttribute("aria-pressed", "false");
-                } else {
-                    hamburger.setAttribute("aria-expanded", "true");
-                    hamburger.setAttribute("aria-pressed", "true");
-            };
-        });
-    }};
+                hamburger.setAttribute("aria-expanded", "false");
+            } else  {
+                hamburger.setAttribute("aria-expanded", "true");
+            }
+        })
+    )};
 
-});
+
+    hamburger.forEach(item => 
+        hamburger.onclick = function () {
+            showMenuTrigger.forEach(menu => 
+            menu.addEventListener('change', function (event) {
+                if (!event.target.checked || !hamburger.getAttribute("aria-expanded") == "true") {
+                    hamburger.setAttribute("aria-expanded", "false");
+                } else  {
+                    hamburger.setAttribute("aria-expanded", "true");
+                }
+            })
+        );
+    });
+
+
+    const getCart = document.querySelectorAll('input[type=checkbox][id=drawer__cart]');
+    const toggleCart = document.getElementById("cart");
+    toggleCart.onclick = function () {
+        for (let cart of getCart) {
+            cart.addEventListener('change', function (event) {
+                if (event.target.checked || toggleCart.getAttribute("aria-expanded") == "false") {
+                    toggleCart.setAttribute("aria-expanded", "true");
+                } else  {
+                    toggleCart.setAttribute("aria-expanded", "false");
+                }
+            });
+        };
+    };
+
+    // const getCustomer = document.querySelectorAll('input[type=checkbox][id=drawer__customer]');
+    // const toggleCustomer = document.getElementById("customer");
+    // toggleCustomer.onclick = function () {
+    //     for (let customer of getCustomer) {
+    //         customer.addEventListener('change', function (event) {
+    //             if (event.target.checked || customer.getAttribute("aria-expanded") == "false") {
+    //                 customer.setAttribute("aria-expanded", "true");
+    //             } else  {
+    //                 customer.setAttribute("aria-expanded", "false");
+    //             }
+    //         });
+    //     };
+    // };
+
+    const getSearch = document.querySelectorAll('input[type=checkbox][id=drawer__search]');
+    const toggleSearch = document.getElementById("search");
+    toggleSearch.onclick = function () {
+        for (let searches of getSearch) {
+            searches.addEventListener('change', function (event) {
+                if (event.target.checked || toggleSearch.getAttribute("aria-expanded") == "false") {
+                    toggleSearch.setAttribute("aria-expanded", "true");
+                } else  {
+                    toggleSearch.setAttribute("aria-expanded", "false");
+                }
+        })
+    }} 
+
+
+})
